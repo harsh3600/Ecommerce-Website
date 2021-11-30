16d923b174d8d0bb8c5a49a7195c8222518071ed
Harsh Kumar <harshkumararmy@gmail.com>
2021-11-30 21:05:40 +0530

    Added new files

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAs_a8FMhiUWfA8XALgIOUT2bqUMbXtZPc",
+    authDomain: "pharma-assignments.firebaseapp.com",
+    projectId: "pharma-assignments",
+    storageBucket: "pharma-assignments.appspot.com",
+    messagingSenderId: "738051166092",
+    appId: "1:738051166092:web:8b0891474ad0c9c29650a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
